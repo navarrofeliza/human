6b57d590ad6810b238e0a2d05594449b308ed1cc
navarrofeliza <navarrofeliza@yahoo.com>
2022-02-08 13:22:07 -0800

    human submission

---
+++ Program.cs
@@ -0,0 +1,45 @@
+﻿namespace Human
+{    
+    class Human
+    {
+        // Fields for Human
+        public string Name;
+        public int Strength;
+        public int Intelligence;
+        public int Dexterity;
+        private int health;
+        
+        // add a public "getter" property to access health
+        public int Health
+        {
+            get {return health;}
+        }
+        // Add a constructor that takes a value to set Name, and set the remaining fields to default values
+        public Human(string name)
+            {
+                Name = name;
+                Strength = 3;
+                Intelligence = 3;
+                Dexterity = 3;
+                health = 100;
+            }
+        // Add a constructor to assign custom values to all fields
+        public Human(string name, int str, int intel, int dex, int hp)
+            {
+                Name = name;
+                Strength = str;
+                Intelligence = intel;
+                Dexterity = dex;
+                health = hp;
+            }
+        // Build Attack method
+        public int Attack(Human target)
+        {
+            int dmg = Strength * 3;
+            target.health -= dmg;
+            Console.WriteLine($"{Name} attacked {target.Name} for {dmg} damage!");
+            return target.health;
+        }
+    }
+}
+
